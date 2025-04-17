91f167e9f7d2f8c0789bc92750d4772b0d7024f4
Avi Chandra <113673662+achandra1319@users.noreply.github.com>
2025-04-17 11:54:08 +0100

    Update data_prep_avi.py

--- data_prep_avi.py
+++ data_prep_avi.py
@@ -31,10 +31,44 @@ df = pd.read_csv(data)
 #We will need code here for extracting the data from our clinical variables from the screening visit (VISCODE2='sc'). 
 
 #Justification
-
 #PHYSICAL Data: 3746 screening cases vs. 289 baseline cases (some of these missing additional data)
 #NEUROEXM Data: 3748 screening cases vs. 289 baseline cases (some of these missing additional data)
-#VITALS Data: 2806 screening cases vs. 2722 baseline cases (many missing height data)
+#VITALS Data: 2806 screening cases vs. 2722 baseline cases (many missing height data - coded as -4)
+
+#Identifier variable: Across all datasets
+#RID (Participant roster ID)
+
+#Screening clinical exam date variable: PHYSICAL, NEUROEXM, VITALS
+#EXAMDATE (Clinical Screening Examination Date)
+
+#Clinical variables: PHYSICAL
+#PXGENAPP (1. General Appearance; 1=Normal, 2=Abnormal)
+#PXHEADEY (2. Head, Eyes, Ears, Nose and Throat; 1=Normal, 2=Abnormal)
+#PXNECK (3. Neck; 1=Normal, 2=Abnormal)
+#PXCHEST (4. Chest; 1=Normal, 2=Abnormal)
+#PXHEART (5. Heart; 1=Normal, 2=Abnormal)
+#PXABDOM (6. Abdomen; 1=Normal, 2=Abnormal)
+#PXEXTREM (7. Extremities; 1=Normal, 2=Abnormal)
+#PXEDEMA (8. Edema; 1=Normal, 2=Abnormal)
+#PXPERIPH (9. Peripheral Vascular; 1=Normal, 2=Abnormal)
+#PXSKIN (10. Skin and Appendages; 1=Normal, 2=Abnormal)
+#PXMUSCUL (11. Musculoskeletal; 1=Normal, 2=Abnormal)
+#PXBACK (12. Back; 1=Normal, 2=Abnormal)
+#PXOTHER (13. Other; 1=Normal, 2=Abnormal)
+
+#Clinical variables: NEUROEXM
+#NEUROEXM (1. General Appearance; 1=Normal, 2=Abnormal)
+
+
+
+
+
+
+#Covariates
+#Age at screening visit
+
+
+
 
 
 
